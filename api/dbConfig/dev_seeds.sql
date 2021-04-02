afd455bfc1d59957284b00299dcc34c71348a635
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-04-02 09:48:56 +0100

    add dev user seeds

---
+++ api/dbConfig/dev_seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (username, password_digest) 
+VALUES
+    ('duckfliesagain', 'tu9ibtoi4tbh2hhuet' ),
+    ('healthman', 'h4hoeuba3r3tbaeu' ),
+    ('notanotherhabittracker', 'ahudnthpdohbapbb243' );
