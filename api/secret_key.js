d6102abf6e434cf9dc1b2506157ff752b9a9eaec
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-04-02 12:36:34 +0100

    add console msg

--- api/secret_key.js
+++ api/secret_key.js
@@ -1,5 +1,8 @@
 // Generation .env secret key
 const fs = require('fs')
 const key = require('crypto').randomBytes(64).toString('hex');
-const file = "../api/.env";
-fs.writeFile(file, `SECRET=${key}`, () => {});
+const file = ".env";
+fs.writeFile(file, `SECRET=${key}`, (err) => {
+    if(err) throw err;
+    console.log('Generated Secret Key (shh!)');
+});
