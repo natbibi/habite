ed52d145d622153cc51ce192683dbbd25c5048bb
chris westerduin <chriswesterduin@outlook.com>
2021-04-04 00:15:24 +0100

    basic rate limiting implemented in server.js

--- api/server.js
+++ api/server.js
@@ -5,6 +5,36 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
+// basic rate limiting to prevent brute forcing
+const rateLimit = require("express-rate-limit");
+
+const limiter = () => rateLimit({
+  windowMs: 5 * 60 * 1000, // 5 minutes
+  max: 3, // limit each IP/username combo to 3 requests per windowMs
+  skipSuccessfulRequests: true, // dont limit on successful requests
+  message: 'too many login attempts',
+  keyGenerator: function(req,res){
+      console.log(req.ip + req.body.username)
+      return req.ip + req.body.username
+  }
+});
+
+const totalLimiter = () => rateLimit({
+    windowMs: 24 * 60 * 60 * 1000, // 1 day
+    max: 100, // limit each IP to 100 requests per windowMs
+    skipSuccessfulRequests: true, // dont limit on successful requests
+    message: 'too many failed requests - try again later',
+    keyGenerator: function(req,res){
+        console.log(req.ip + req.body.username)
+        return req.ip
+    }
+  });
+
+// apply to all routes
+server.use(totalLimiter())
+// apply to only login requests
+server.use("/auth/login", limiter());
+
 const usersRoutes = require('./routes/users');
 const habitsRoutes = require('./routes/habits');
 const authRoutes = require('./routes/auth')
