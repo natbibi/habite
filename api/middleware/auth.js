c0db411a402d530a30d7fc61d26e35caea145dea
natbibi <58271566+natbibi@users.noreply.github.com>
2021-04-03 14:22:47 +0100

    Merge branch 'staging' of https://github.com/natbibi/co-habiters into client-natalie

---
+++ api/middleware/auth.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+
+function verifyToken(req, res, next){
+    const header = req.headers['authorization'];
+    if (header) {
+        const token = header.split(' ')[1];
+        jwt.verify(token, process.env.SECRET, async (err, data) => {
+            //set a response local variables for user specific authentication
+            res.locals.user = data.username
+            if(err){
+                res.status(403).json({ err: 'Invalid token' })
+            } else {
+                next();
+            }
+        })
+    } else {
+        res.status(403).json({ err: 'Missing token' })
+    }
+}
+module.exports = {
+    verifyToken
+}
