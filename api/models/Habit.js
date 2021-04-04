20bdd3257f5b1d1d8bacb2054a3311d369af9a97
“AdilIqbal95” <adil.iqbal@hotmail.co.uk>
2021-04-04 20:38:52 +0100

    created 'Habit' class (model)

--- api/models/Habit.js
+++ api/models/Habit.js
@@ -1,3 +1,35 @@
 const db = require('../dbConfig/init');
 const SQL = require('sql-template-strings');
 
+class Habit {
+  constructor(data) {
+    this.id = data.id;
+    this.habitName = data.name;
+  }
+
+  static find(habitName) {
+    return new Promise (async (resolve,reject) => {
+      try{
+        const result = await db.query(SQL`SELECT * FROM habits WHERE    name = ${habitName};`);
+        const habit = new Habit(result.rows[0]);
+        resolve(habit)
+      } catch (error) {
+        reject(`Could not retrieve habit`);
+      }
+    });
+  }
+
+  static create(habitName) {
+    return new Promise (async (resolve,reject) => {
+      try{
+        const result = await db.query(SQL`INSERT INTO habits (name) VALUES (${habitName}) RETURNING *;`);
+        const habit = new Habit(result.rows[0]);
+        resolve(habit)
+      } catch (error) {
+        reject(`Could not create habit`);
+      }
+    });
+    
+  }
+  
+}
