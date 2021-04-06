08a60475435c3b3f39eadb5e1c6092bc257c1559
natbibi <58271566+natbibi@users.noreply.github.com>
2021-04-06 16:26:04 +0100

    change to local host for dev

--- client/js/requests.js
+++ client/js/requests.js
@@ -3,7 +3,8 @@ async function getAllHabits() {
         const options = {
             headers: new Headers({ 'Authorization': localStorage.getItem('token') }),
         }
-        const response = await fetch('https://habit-your-way.herokuapp.com/habits', options);
+        const response = await fetch('http://localhost:3000/habits', options);
+        // https://habit-your-way.herokuapp.com/habits 
         const data = await response.json();
         if (data.err) {
             console.warn(data.err);
