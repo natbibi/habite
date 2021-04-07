aa234fc6cbde20e94c93e13df8db9b3d7ae40294
natbibi <58271566+natbibi@users.noreply.github.com>
2021-04-07 11:05:29 +0100

    add decrementHabit DELETE req

--- client/bundle.js
+++ client/bundle.js
@@ -497,14 +497,13 @@ module.exports = {
 },{}],7:[function(require,module,exports){
 const auth = require('./auth')
 const hostURL = "http://localhost:3000";
+const username = auth.currentUser();
 
 async function getAllHabits() {
     try {
         const options = {
             headers: new Headers({ 'Authorization': localStorage.getItem('token') }),
         }
-        const username = auth.currentUser()
-        console.log(username)
         const response = await fetch(`${hostURL}/users/${username}/habits/entries`, options)
         // https://habit-your-way.herokuapp.com/habits 
         const data = await response.json();
@@ -514,6 +513,19 @@ async function getAllHabits() {
             logout();
         }
         return data;
+    } catch (err) {
+        console.warn(err);
+    }
+}
+
+async function decrementHabit(id){
+    try {
+        const options = { 
+            method: 'DELETE',
+            headers: new Headers({ 'Authorization': localStorage.getItem('token') }), 
+        }
+        await fetch(`${hostURL}/users/${username}/habits/entries/${id}`, options);
+        window.location.hash = `#profile`
     } catch (err) {
         console.warn(err);
     }
