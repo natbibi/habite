8f863400f4ed0df12434a577ade6b9dcb2f746ad
Elwin <elwin.carlos09@gmail.com>
2021-04-07 12:29:18 +0100

    add createNewHabitForm to addHabits

--- client/js/addHabits.js
+++ client/js/addHabits.js
@@ -49,24 +49,55 @@ async function renderAddHabitsPage() {
     main.appendChild(addHabitForm)
 }
 
-async function addHabitForm() {
-   // add a new user habit from a list
+// async function addHabitForm() {
+//    // add a new user habit from a list
      
-     return addHabitForm;
-}
+//      return addHabitForm;
+// }
 
-async function createNewHabitForm() {
-    // create a new habit and add it to habits list (POST) 
+/*async*/ function createNewHabitForm() {
+    // create a new habit and add it to habits list (POST)
+    
+    // Target the main element (add heading for testing);
+    const heading = document.querySelector('header');
+    const main = document.querySelector('main');
+    // Create a new div for adding elements
+    let newHabitDiv = document.createElement('div');
+    // Create a label for adding a new habit
+    let newHabitLabel = document.createElement('label');
+    newHabitLabel.for = "new-habit";
+    newHabitLabel.textContent = "Or add a custom one!";
+    // Create a text area for the user to create their new habit
+    let newHabit = document.createElement('input');
+    newHabit.type = "text";
+    newHabit.name = "new-habit";
+    newHabit.id = "new-habit";
+    // Create a submit button for the new habit
+    let button = document.createElement('button');
+    // Create an icon for the submit button
+    let buttonIcon = document.createElement('i');
+    buttonIcon.className = "fas fa-plus-circle";
+    // Append the icon to the button
+    button.appendChild(buttonIcon);
+    // Append the newHabit features to the div
+    newHabitDiv.appendChild(newHabitLabel);
+    newHabitDiv.appendChild(newHabit);
+    newHabitDiv.appendChild(button);
+    // Append everything to main
+    main.appendChild(newHabitDiv);
 
+    return main;
+
+    // Desired HTML content below:
 //     <div class="add-habit-input">
 //     <label for="new-habit">Or add a custom one!</label>
 //     <input type="text" name="new-habit" id="new-habit">
 //     <button><i class="fas fa-plus-circle"></i></button>
-// </div>
+//     </div>
 }
 
 async function deleteHabitForm() {
     // delete a user habit so it is no longer tracked
 }
 
-module.exports = {renderAddHabitsPage};
+module.exports = { renderAddHabitsPage, createNewHabitForm };
