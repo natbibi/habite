f1ecee1876e7476bedaa7e1905fb8ce002d67297
natbibi <58271566+natbibi@users.noreply.github.com>
2021-04-07 11:06:02 +0100

    remove getAllUsers and add decrementHabit to module exports

--- client/js/requests.js
+++ client/js/requests.js
@@ -34,21 +34,4 @@ async function decrementHabit(id){
     }
 }
 
-async function getAllUsers() {
-    try {
-        const options = {
-            headers: new Headers({ 'Authorization': localStorage.getItem('token') }),
-        }
-        const response = await fetch('https://habit-your-way.herokuapp.com/users', options);
-        const data = await response.json();
-        if (data.err) {
-            console.warn(data.err);
-            logout();
-        }
-        return data;
-    } catch (err) {
-        console.warn(err);
-    }
-}
-
-module.exports = { getAllHabits, getAllUsers }
+module.exports = { getAllHabits, decrementHabit }
