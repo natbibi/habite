ac18ec641f9345418d6f8c9b5249fcb5c02eddad
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-04-07 08:52:42 +0100

    add production switch for db connection

--- api/dbConfig/init.js
+++ api/dbConfig/init.js
@@ -1,12 +1,18 @@
 const { Pool } = require("pg");
 const connectionString = process.env.DATABASE_URL
 
-const pool = new Pool({
-    connectionString,
-    ssl: {
-      rejectUnauthorized: false
-    }
-})
+const env = process.env.NODE_ENV;
+
+let options;
+
+if (env === "production") {
+  options = { connectionString, ssl: { rejectUnauthorized: false } }
+} else {
+  options = { connectionString }
+}
+
+const pool = new Pool(options);
+
 module.exports = {
   async query(text, params) {
     const start = Date.now();
