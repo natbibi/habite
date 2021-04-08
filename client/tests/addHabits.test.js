94f3b6e544200b3d66d1515776d01e905ebcdca8
Rishi <rishi_bhugobaun@yahoo.co.uk>
2021-04-08 17:00:10 +0100

    add add habit page tests

--- client/tests/addHabits.test.js
+++ client/tests/addHabits.test.js
@@ -1,25 +1,108 @@
-let addHabits;
+const fs = require('fs');
+const path = require('path');
+const html = fs.readFileSync(path.resolve(__dirname, '../index.html'), 'utf8');
+
+
+let addPage;
+
+
+// const auth = require('../js/auth');
+
+jest.mock('../js/requests');
+const req = require('../js/requests');
+// const forms = require('../js/forms');
+// jest.mock('../js/forms');
+// jest.mock('../js/auth');
 
 describe('addHabits', () => {
     let header;
     let main;
 
     beforeAll(() => {
-        document.documentElement.innerHTML = '<header id="top"></header><main class="container"></main>';
+        document.documentElement.innerHTML = html.toString();
+        // `
+        // <header id="top"></header>
+        // <main class="container"></main>
+        // <footer></footer>`;
+        
         heading = document.querySelector('header');
         main = document.querySelector('main');
-        addHabits = require('../js/addHabits');
+        addPage = require('../js/addHabits');
     });
 
     beforeEach(() => {
         heading.innerHTML = '';
         main.innerHTML = '';
-        addHabits.renderAddHabitsPage();
-        addHabits.createNewHabitForm();
+        // addHabits.renderAddHabitsPage();
+        // addHabits.createNewHabitForm();
     });
+
+    describe('createAddHabitForm', () => {
+        let form;
+        beforeAll(() => {
+            form = addPage.createAddHabitForm();
+        });
+
+        it('should return a form', () => {
+            expect(form).toBeInstanceOf(HTMLFormElement);
+        });
+
+        it('should make a post request on form submit with a habit id and a frequency', () => {
+            req.postData = jest.fn();
+            form.submit();
+    
+            expect(req.postData).toHaveBeenCalled();
+        });
+
+    });
+
+    describe('createNewHabitForm', () => {
+        let form;
+        beforeAll(() => {
+            form = addPage.createNewHabitForm();
+        });
+
+        it('should return a form', () => {
+            const form = addPage.createNewHabitForm();
+            expect(form).toBeInstanceOf(HTMLFormElement);
+        });
+
+        it('should make a post request on form submit', () => {
+            req.postData = jest.fn();
+            form.submit();
+    
+            expect(req.postData).toHaveBeenCalled();
+        });
+
+    });
+
+    describe('createDeleteHabitForm', () => {
+        let form;
+        beforeAll(() => {
+            form = addPage.createDeleteHabitForm();
+        });
+
+        it('should return a form', () => {
+            const form = addPage.createDeleteHabitForm();
+            expect(form).toBeInstanceOf(HTMLFormElement);
+        });
+
+        it('should make a delete request on form submit', () => {
+            req.deleteData = jest.fn();
+            form.submit();
+    
+            expect(req.deleteData).toHaveBeenCalled();
+        });
+    });
+
 
     describe('renderAddHabitsPage', () => {
 
+        it('should add the add, create and delete habit forms to the main area', () => {
+            addPage.renderAddHabitsPage()
+            const forms = document.querySelectorAll("form");
+            expect(forms).toHaveLength(3);
+        });
         // it('heading should contain a greeting with a prompt to get started', () => {
         //     let greeting = heading.querySelector('h1');
         //     expect(greeting).toBeTruthy();
@@ -30,21 +113,6 @@ describe('addHabits', () => {
         //     let form = main.querySelector('form');
         //     expect(form).toBeTruthy();    
         // });
-
-    });
-
-    describe('createNewHabitForm', () => {
-
-        it('should have a text area', () => {
-            let textArea = main.querySelector('input');
-            expect(textArea).toBeTruthy();
-            expect(textArea.type).toContain('text');     
-        });
-
-        it('should have a button', () => {
-            let button = main.querySelector('input');
-            expect(button).toBeTruthy();      
-        });
 
     });
 
